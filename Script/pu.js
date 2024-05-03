e7b1afd6a68e845db1fbbd81e97120fe5125c603
Gyspops <excaliburv@outlook.com>
2024-05-03 17:24:31 +0800

    Script

---
+++ Script/pu.js
@@ -0,0 +1,8 @@
+let obj = JSON.parse($response.body);
+delete obj.content.place_ad_1;
+delete obj.content.place_ad_3;
+delete obj.content.place_ad_2;
+delete obj.content.articles;
+let modifiedBody = JSON.stringify(obj);
+console.log(modifiedBody);
+$done({body: modifiedBody});
